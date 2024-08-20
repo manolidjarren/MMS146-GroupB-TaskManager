48d4f08305a2a243b10f1a1f656e0240ca05a65d
spamores <spamores@up.edu.ph>
2024-08-21 01:41:32 +0800

    Update task.py
    i made some updates to the task class mostly based on @AllanaAngeo's suggestions.here's what i've done:
    1. added priority levels as  numeric values, completion
    2. added 3 status option to represent task progress. (initially, i tried to set the completion status to percentage value as indicated in the proj specifications pero di ko kinaya haha so ginaya ko na lang yung status sa gdocs natin. but this can always be adjusted as needed)
    3. description, priority and completion status methods
    let me know if there are any concerns!

--- GroupB_TaskManagerProgram/task.py
+++ GroupB_TaskManagerProgram/task.py
@@ -1,18 +1,33 @@
 import datetime
 
 class Task:
-    def __init__(self, task_name, due_date):
+
+    #priority levels
+    PRIORITY_LOW = 0
+    PRIORITY_MEDIUM = 1
+    PRIORITY_HIGH = 2
+
+    # Completion statuses
+    STATUS_NOT_STARTED = "Not Started"
+    STATUS_IN_PROGRESS = "In progress"
+    STATUS_COMPLETED = "Completed"
+
+    def __init__(self, task_name, description, due_date, priority_level=PRIORITY_LOW, completion_status=0.0):
         self.task_name = task_name
-        self.description =  ""
+        self.description =  description
         self.due_date = due_date
-        self.priority_level = None
+        self.priority_level = priority_level
         #could also be 0 like set priority level numerically then assign keywords low, medium. and high to numerical value
         #OR para sa sort tasks by priority sa TaskManager class, we can first arrange each low, medium, and high priority by due date,
         #and then print low tasks, then medium, then high. Maybe we need a list for this idunno gl task class people
-        self.completion_status = "" #Ongoing or Completed ganern
-    def set_description ():
-        pass
-    def set_priority ():
-        pass
-    def set_completion_status ():
-        pass
+        self.completion_status = completion_status #Ongoing or Completed ganern
+    
+    #update the description of the task
+    def set_description (self, description): 
+        self.description = description
+    #update the priority of the task (numeric value)
+    def set_priority (self, priority_level):
+        self.priority_level = priority_level
+    #update the completion status of the task 
+    def set_completion_status (self, completion_status):
+        self.completion_status = completion_status
