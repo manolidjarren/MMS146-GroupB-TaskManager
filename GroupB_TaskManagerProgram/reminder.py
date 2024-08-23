15348e8c2c7bf9dfdcd9e225392d0d7f635ad913
antonimercado <153090907+antonimercado@users.noreply.github.com>
2024-08-23 12:56:56 +0800

    Updated reminder.py
    I changed a few things in Aidan's code:
    - Enhanced the reminder generation
     * it now has a post deadline reminder
    - separated the time calculation
    
    For the tester, since I updated the reminder generation, I used a past and future case.

--- GroupB_TaskManagerProgram/reminder.py
+++ GroupB_TaskManagerProgram/reminder.py
@@ -1,19 +1,38 @@
 from datetime import date
 
 class Reminder(Task):
-    def __init__ (self, time_until_deadline, notifications):
-      super().__init__(task_name, description, due_date, priority_level, completion_status)  
-        self.time_until_deadline = time_until_deadline
+    def __init__(self, task_name, due_date, notifications):
+        super().__init__(task_name, due_date)
+        self.time_until_deadline = self.calculate_time_until_deadline()
         self.notifications = notifications
-    def generate_reminder (self):
-        #date format YYYY/MM/DD
-        current_date = date.today()
-        difference = self.time_until_deadline - current_date
-        print(f"The task is due in {difference.days} days")
-    def display_notif ():
-        pass
 
-#test instance lang para macheck nyo sa Visual Code pano gumagana
-#sa instance mo rin ilalagay yung deadline ng task in year/month/day format
-test = Reminder (date(2024, 8, 30), "N/a")
-test.generate_reminder()
+    def calculate_time_until_deadline(self):
+        current_date = date.today()
+        return self.due_date - current_date 
+
+    def generate_reminder (self):
+        difference = self.time_until_deadline
+        if difference.days >= 0:
+            print(f"The task is due in {difference.days} days")
+        else: 
+            print(f"The task {self.task_name} was due {-difference.days} days ago.")
+
+    def display_notif(self):
+        print(self.notifications)
+
+reminder = Reminder(
+    task_name = "Task Manager Group Project",
+    due_date=date(2024,9,1),
+    notifications="Reminder: Submit the group project!"
+)
+
+reminder2 = Reminder(
+    task_name = "KAS 1: Draft your Essay Answers",
+    due_date=date(2024,8,22),
+    notifications="Complete it as soon as possible!"
+)
+
+reminder.generate_reminder()
+reminder.display_notif()
+reminder2.generate_reminder()
+reminder2.display_notif()
