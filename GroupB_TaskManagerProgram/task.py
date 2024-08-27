664784430e08a6d643de0563cda90bfa1101bc2f
manolidjarren <jarren.manolid195@gmail.com>
2024-08-28 07:03:08 +0800

    Update task.py
    changes
    - fixed the priority and completion status for sorting sa taskmanager. 
    
    - made all the update functions for the task manager in the task class nalang (set_due_date, set_description, etc. )-  ung edit_task functionsa taskmanager class kase gets really messy agad - its best to use this para flexible and efficient

--- GroupB_TaskManagerProgram/task.py
+++ GroupB_TaskManagerProgram/task.py
@@ -1,39 +1,81 @@
-import datetime
+from datetime import date
 
 class Task:
 
-    #priority levels
-    PRIORITY_LOW = 0
-    PRIORITY_MEDIUM = 1
-    PRIORITY_HIGH = 2
-
-    # Completion statuses
-    STATUS_NOT_STARTED = "Not Started"
-    STATUS_IN_PROGRESS = "In Progress"
-    STATUS_COMPLETED = "Completed"
-
-    def __init__(self, task_name, description, due_date, priority_level=PRIORITY_LOW, completion_status=STATUS_NOT_STARTED):
+    def __init__(self, task_name, description, due_date, priority_level=0, completion_status="Not Started"):
         self.task_name = task_name
         self.description =  description
         self.due_date = due_date
-        self.priority_level = priority_level
-        #could also be 0 like set priority level numerically then assign keywords low, medium. and high to numerical value
-        #OR para sa sort tasks by priority sa TaskManager class, we can first arrange each low, medium, and high priority by due date,
-        #and then print low tasks, then medium, then high. Maybe we need a list for this idunno gl task class people
-        self.completion_status = completion_status #Ongoing or Completed ganern
-    
+        self.set_priority(priority_level)           #This is used to print out the Priority Level into Words 
+        self.priority_level_num = 0                 #This will be used for Sorting out the Priorities in TaskManager
+        self.completion_status = completion_status  #Ongoing or Completed ganern
+        self.completion_status_num = 0              #This will be used for Sorting out the Status in TaskManager
+    #update the name of the task
+    def set_task_name (self, new_task_name):
+        #Checks if the input is a string.
+        if isinstance(new_task_name,str):
+            return self.task_name
+        else:
+            print("Error Occured, Please Type the Description in Strings")
+
     #update the description of the task
     def set_description (self, new_description):
-        if type(new_description) == str: #checks if the value is a valid type for this attribute
-            self.description = new_description
-    #update the priority of the task (numeric value)
-    def set_priority (self, new_priority_level):
-        if type(new_priority_level) == int:
-            if new_priority_level <= 2 & new_priority_level >= 0: #only changes value if within a certain range
-                self.priority_level = new_priority_level
+        #Checks if the input is a string.
+        if isinstance(new_description,str):
+            return self.description
+        else:
+            print("Error Occured, Please Type the Description in Strings")
+
+    def set_due_date (self, new_date):
+        #Checks if the input is in datetime.
+        current_date = date.today()
+        if isinstance(new_date, date.datetime):
+            #Checks if the New Due Date is already passed the Current Day. 
+            if new_date <= current_date:
+                return self.due_date
+        else:
+            print("Error Occured, Please Type the New Due Date")
+   
+    def set_priority(self, new_priority_level):
+         #Checks if the input is in numbers(integers)
+        if isinstance(new_priority_level, int):
+            # Mapping the numbers in the the Priority Levels.         
+            priority_mapping = {
+                0: "LOW PRIORITY",
+                1: "MID PRIORITY",
+                2: "HIGH PRIORITY"
+            }
+            if new_priority_level in priority_mapping:
+                self.priority_level = priority_mapping[new_priority_level]
+                self.priority_level_num = new_priority_level #This will be used for sorting 
+            #Else, prints out error
+            else:
+                print("Error Occured, Please pick from 0-2")
+
     #update the completion status of the task 
     def set_completion_status (self, new_completion_status):
-        if type(new_completion_status) == str:
-            status = ["Not Started", "In Progress", "Completed"]
-            if any(s == new_completion_status for s in status): #only changes value if its any of the valid statuses
+        status_mapping = {
+                "Not Started": 2,
+                "In Progress": 1,
+                "Completed" : 0 
+            }
+        if isinstance(new_completion_status,str):
+            if new_completion_status in status_mapping: #only changes value if its any of the valid statuses
                 self.completion_status = new_completion_status
+                self.completion_status_num = status_mapping[new_completion_status]
+        else: 
+            print('Error Occured, Please pick from "Not Started", "In Progress", and "Completed"')
+
+    #Info of Tasks, and used for checking the Attributes.
+    def task_info (self):
+            task_details = {
+                "Name": self.task_name,
+                "Description": self.description,
+                "Due Date": self.due_date,
+                "Priority": self.priority_level,
+                "Priority in Number": self.priority_level_num,
+                "Completion Status": self.completion_status,
+                "Completion Status in Number": self.completion_status_num
+
+            }
+            return task_details
