3c01e8c01cf7f998ab51c2736dabfb8baf9fc3e8
antonimercado <153090907+antonimercado@users.noreply.github.com>
2024-08-27 22:39:13 +0800

    Update taskmanager.py
    Added self in some of the functions

--- GroupB_TaskManagerProgram/taskmanager.py
+++ GroupB_TaskManagerProgram/taskmanager.py
@@ -1,12 +1,12 @@
 class TaskManager(Task):
     def __init__ (task_name, description, due_date, priority_level, completion_status):
         super ().__init__(task_name, description, due_date, priority_level, completion_status)
-        self.__tasklist = tasklist
-    def add_task ():
+        self.__tasklist = []
+    def add_task (self):
         self.__tasklist.append(Task)
     def edit_task ():
         pass
-    def delete_task ():
+    def delete_task (self):
         self.__tasklist.remove(Task)
     def view_tasks ():
         pass
